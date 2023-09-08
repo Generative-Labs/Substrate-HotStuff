c592698534772939b045d1bc5c606ea76f9398da
sunjiaxing <15215616197@163.com>
2023-09-08 12:09:07 +0800

    1、solve FinalityBlock grammar problems

--- hotstuff/src/finalize_block.rs
+++ hotstuff/src/finalize_block.rs
@@ -6,7 +6,6 @@ use sc_client_api::{
     CallExecutor,
 };
 use sc_consensus_grandpa::{
-    // LinkHalf,
     ClientForGrandpa,
 };
 use sp_blockchain::Error;
@@ -15,18 +14,17 @@ use sp_runtime::{
     traits::{Block as BlockT, NumberFor},
 };
 
-// use sc_consensus_grandpa::authorities::SharedAuthoritySet;
-// use sc_service::client::Client;
-
-pub struct FinalityBlock<B, E, Block, RA, Client> //, C, SC>
+pub struct FinalityBlock<B, E, Block, RA, Client>
     where
         B: backend::Backend<Block>,
         E: CallExecutor<Block>,
         Block: BlockT,
-
 {
-    client: Arc<Client<B, E, Block, RA>>,
-    // grandpalink: LinkHalf<Block, C, SC>,
+    client: Arc<Client>, // Use the generic Client type here
+    _unused_b: std::marker::PhantomData<B>,
+    _unused_e: std::marker::PhantomData<E>,
+    _unused_block: std::marker::PhantomData<Block>,
+    _unused_ra: std::marker::PhantomData<RA>,
 }
 
 impl<B, E, Block, RA, Client> FinalityBlock<B, E, Block, RA, Client>
@@ -36,8 +34,14 @@ impl<B, E, Block, RA, Client> FinalityBlock<B, E, Block, RA, Client>
         E: CallExecutor<Block>,
         Client: ClientForGrandpa<Block, B>,
 {
-    pub fn new(client: Arc<Client<B, E, Block, RA>>) -> Self {
-        FinalityBlock { client }
+    pub fn new(client: Arc<Client>) -> Self {
+        FinalityBlock {
+            client,
+            _unused_b: Default::default(),
+            _unused_e: Default::default(),
+            _unused_block: Default::default(),
+            _unused_ra: Default::default(),
+        }
     }
 
     pub fn update_state(&self, _block_hash: Block::Hash, _block_number: NumberFor<Block>) {
@@ -51,7 +55,6 @@ impl<B, E, Block, RA, Client> FinalityBlock<B, E, Block, RA, Client>
         notify: bool,
     ) -> Result<(), Error> {
         // TODO: Implement the application of finality to the blockchain logic
-        // self.grandpalink.client.finalize_block(block_hash, persisted_justification, notify)?;
         self.client.finalize_block(block_hash, persisted_justification, notify)?;
         Ok(())
     }
